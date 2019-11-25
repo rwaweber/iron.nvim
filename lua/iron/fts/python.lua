a2109c137811c95b08e3a2bac071d6086d637860
Henry Kupty <hkupty@gmail.com>
2019-11-25 21:34:24 +0100

    New attempt: Add \27 (escape) before line end
    This should work on windows.
    Doesn't affect linux.

--- lua/iron/fts/python.lua
+++ lua/iron/fts/python.lua
@@ -2,16 +2,16 @@ local extend = require("iron.util.tables").extend
 local lf = require("iron.fts.common").lf
 local python = {}
 
-local format = function(open, close, cr)
+local format = function(open, close, endline)
   return function(lines)
     if #lines == 1 then
-      return { lines[1] .. cr }
+      return { lines[1] .. endline }
     else
       local new = { open .. lines[1] }
       for line=2, #lines do
         table.insert(new, lines[line])
       end
-      return extend(new, close)
+      return extend(new, close .. endline)
     end
   end
 end
@@ -19,7 +19,7 @@ end
 local def = function(cmd)
   return {
     command = cmd,
-    format = format("\27[200~", "\27[201~" .. lf, lf)
+    format = format("\27[200~", "\27[201~", "\27" .. lf)
   }
 end
 
